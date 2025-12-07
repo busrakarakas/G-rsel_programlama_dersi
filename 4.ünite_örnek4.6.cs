658252bdc883d52df5faf1d2577bdcab03d54c8e
Büşra <karakasbusrasu@gmail.com>
2025-12-07 17:54:31 +0300

    Create 4.ünite_örnek4.6.cs

---
+++ 4.ünite_örnek4.6.cs
@@ -0,0 +1,17 @@
+class Account
+{
+   private string name; // instance variable
+
+   // property to get and set the name instance variable               
+   public string Name
+   {
+      get // returns the corresponding instance variable's value
+      {
+         return name; // returns the value of name to the client code
+      }
+      set // assigns a new value to the corresponding instance variable
+      {
+         name = value; // value is implicitly declared and initialized
+      }
+   }
+}
