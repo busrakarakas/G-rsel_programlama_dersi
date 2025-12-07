668e14c38f56030c28d0965c545e4c7c04780be7
Büşra <karakasbusrasu@gmail.com>
2025-12-07 17:51:39 +0300

    Create 3.ünite_örnek3.10.cs

---
+++ 3.ünite_örnek3.10.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Welcome2
+{
+   // Main method begins execution of C# app
+   static void Main()
+   {
+      Console.Write("Welcome to ");
+      Console.WriteLine("C# Programming!");
+   } // end Main
+} 
