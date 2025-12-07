4cbd65dc2efb44e662f5a90d9b36e2f1ae592324
Büşra <karakasbusrasu@gmail.com>
2025-12-07 18:00:12 +0300

    Create 6.ünite_örnek6.7.cs
    Implement a do-while loop that counts from 1 to 10.

---
+++ 6.ünite_örnek6.7.cs
@@ -0,0 +1,17 @@
+using System;
+
+class DoWhileTest
+{
+   static void Main()
+   {
+      int counter = 1; // initialize counter
+
+      do
+      {
+         Console.Write($"{counter}  ");
+         ++counter;
+      } while (counter <= 10); // required semicolon 
+
+      Console.WriteLine();
+   } 
+}
