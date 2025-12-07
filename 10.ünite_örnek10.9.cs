e60b23e2e43da7de50a7413834e8dc50fabfefa8
Büşra <karakasbusrasu@gmail.com>
2025-12-07 17:56:09 +0300

    Create 10.ünite_örnek10.9.cs

---
+++ 10.ünite_örnek10.9.cs
@@ -0,0 +1,13 @@
+using System;
+
+class EmployeeTest
+{
+   static void Main()
+   {
+      var birthday = new Date(7, 24, 1949);
+      var hireDate = new Date(3, 12, 1988);
+      var employee = new Employee("Bob", "Blue", birthday, hireDate);
+
+      Console.WriteLine(employee);
+   }
+}
