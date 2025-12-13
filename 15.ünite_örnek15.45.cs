b1e425e8215d07c57ec5aed575cc3ab1ba2967ba
Büşra <karakasbusrasu@gmail.com>
2025-12-13 20:15:51 +0300

    Create 15.ünite_örnek15.45.cs

---
+++ 15.ünite_örnek15.45.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Windows.Forms;
+
+namespace VisualInheritanceBase
+{
+   // base Form used to demonstrate visual inheritance
+   public partial class VisualInheritanceBaseForm : Form
+   {
+      // constructor
+      public VisualInheritanceBaseForm()
+      {
+         InitializeComponent();
+      }
+
+      // display MessageBox when Button is clicked
+      private void learnMoreButton_Click(object sender, EventArgs e)
+      {
+         MessageBox.Show(
+            "Bugs, Bugs, Bugs is a product of deitel.com",
+            "Learn More", MessageBoxButtons.OK,
+            MessageBoxIcon.Information);
+      }
+   }
+}
